8f9ed8e775a7affd66620d1158e41a3bf18966c0
Rohan <gateway2rohan@gmail.com>
2020-10-01 16:18:49 +0530

    Add String to Integer solution

---
+++ String/string_to_integer.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int myAtoi(String str) {
+        int i = 0;
+        int sign = 1;
+        int result = 0;
+        if (str.length() == 0)
+            return 0;
+
+        // Discard whitespaces in the beginning
+        while (i < str.length() && str.charAt(i) == ' ')
+            i++;
+
+        // Check if optional sign if it exists
+        if (i < str.length() && (str.charAt(i) == '+' || str.charAt(i) == '-'))
+            sign = (str.charAt(i++) == '-') ? -1 : 1;
+
+        // Build the result and check for overflow/underflow condition
+        while (i < str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+            if (result > Integer.MAX_VALUE / 10
+                    || (result == Integer.MAX_VALUE / 10 && str.charAt(i) - '0' > Integer.MAX_VALUE % 10)) {
+                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            result = result * 10 + (str.charAt(i++) - '0');
+        }
+        return result * sign;
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String str = "hacktoberfest 2020";
+        System.out.println(solution.myAtoi(str));
+    }
+}
