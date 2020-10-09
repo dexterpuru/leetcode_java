394a6c093d622775a04728e6520dab87729158ce
Utkarsh Jain <56599295+BittuRoxs@users.noreply.github.com>
2020-10-09 12:55:42 +0530

    Update Solution.java

--- Array/Jump Game/Solution.java
+++ Array/Jump Game/Solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean canJump(int[] nums) {
- 	 if(nums[0]==0){
+ 	 if(nums[0]==0){		//checking the first value for zero
             if(nums.length == 1)
                 return true;
             else
@@ -8,11 +8,12 @@ class Solution {
         }
 	
 	for(int i=0; i< nums.length-1; i++){
-            if(nums[i] == 0){
+            if(nums[i] == 0){		//checking for other zero values
                 int x= 2;boolean flag=true;
                 
 		for(int j=i-1; j>=0; j--){
-
+					//encountered a zero value 
+					//now going back and checking if it is bypassable
                     if(nums[j] < x)
                     { flag=false;}
                     else
